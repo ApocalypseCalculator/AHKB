1de032b5328131eb104dd9070d2986d44b693e7e
Patrick Lin <apocalypsecalculator@gmail.com>
2022-06-22 00:44:37 -0400

    more dynamic loading

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"fmt"
+	"math/rand"
+	"strconv"
 	"time"
 
+	"github.com/faiface/beep"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
 )
 
-//go:embed sauce/001.mp3
+//go:embed sauce/*
 var df embed.FS
 
 const (
@@ -17,17 +21,28 @@ const (
 )
 
 func main() {
-	f, _ := df.Open("sauce/001.mp3")
-	streamer, format, _ := mp3.Decode(f)
-	defer streamer.Close()
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	debug := flag.Bool("debug", false, "enable output")
+	flag.Parse()
+
+	dir, _ := df.ReadDir("sauce")
+	saucestreamers := make([]beep.StreamSeekCloser, 0)
+	for _, file := range dir {
+		f, _ := df.Open("sauce/" + file.Name())
+		streamer, format, _ := mp3.Decode(f)
+		defer streamer.Close()
+		speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+		saucestreamers = append(saucestreamers, streamer)
+	}
 	kl := Keylogger{}
 	for {
 		key := kl.GetKey()
 		if !key.Empty {
-			fmt.Println(key.Rune)
-			speaker.Play(streamer)
-			streamer.Seek(0)
+			rngval := rand.Intn(len(saucestreamers))
+			if *debug {
+				fmt.Println(strconv.Itoa(key.Keycode) + "   #" + strconv.Itoa(rngval))
+			}
+			speaker.Play(saucestreamers[rngval])
+			saucestreamers[rngval].Seek(0)
 		}
 		time.Sleep(delayKeyfetchMS * time.Millisecond)
 	}
